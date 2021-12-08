3d34d3731e9c00d2530229af6ad8cad64f87dff3
DudiAM19 <dudiali79@gmail.com>
2021-12-09 01:04:09 +0700

    nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="body" v-for="item in all" :key="item.id">
+      <h1>{{ item.id }}. {{ item.name }}</h1>
+      <h4>{{ item.email }}</h4>
+    </div>
+  </div>
+</template>
+
+<style>
+.body {
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      all: [],
+    };
+  },
+  methods: {
+    async getData() {
+      const res = await this.$axios.$get(
+        "https://61981d74164fa60017c22f73.mockapi.io/contact"
+      );
+      this.all = res;
+      console.log(res);
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
+</script>
